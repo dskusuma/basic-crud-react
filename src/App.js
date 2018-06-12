b3c510433965c8416fb59ec5f308d7ef22860780
dskusuma <dwisatriakusuma4@gmail.com>
2018-06-12 11:08:27 +0700

    add funcSubmitPost

--- src/App.js
+++ src/App.js
@@ -7,11 +7,44 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
-      title: 'BASIC CRUD APP'
+      title: 'BASIC CRUD APP',
+      act: 0,
+      index:'',
+      postData:[]
+    }
+  }
+
+  funcSubmitPost = (e) => {
+    e.preventDefault();
+    console.log('try');
+
+    let postData = this.state.postData;
+    let userID = this.refs.userID.value;
+    let userPost = this.refs.userPost.value;
+
+    let currentData = {
+      userID, userPost
     }
 
+    postData.push(currentData);
+
+    this.setState({
+      postData: postData
+    })
+
+    this.refs.appForm.reset();
+
+  }
+
+  funcRemovePost = (key_i) => {
+    let currentData = this.state.postData;
+    currentData.splice(key_i,1);
+    this.setState({
+      postData: currentData
+    })
   }
   render() {
+    let postData = this.state.postData;
     return (
       <div className="App">
         <header className="App-header">
@@ -27,8 +60,17 @@ class App extends Component {
         <form ref="appForm" className="appForm">
           <input type="text" ref="userID" placeholder="your ID" className="formField" />
           <input type="text" ref="userPost" placeholder="What do you think..." className="formField" />
-          <button onClick={this.submitPost} className="submitButton"> Post </button>
+          <button onClick={this.funcSubmitPost} className="submitButton"> Post </button>
         </form>
+        <pre>
+          {postData.map((dt, key_i) => 
+            <li key={key_i} className="appList">
+              {key_i+1}.{dt.userID}, {dt.userPost}
+              <button onClick={() => this.funcRemovePost(key_i)} className="removeButton"> Remove </button>
+              <button onClick={() => this.funcEditPost(key_i)} className="editButton"> Edit </button>
+            </li>
+          )}
+        </pre>
       </div>
     );
   }
